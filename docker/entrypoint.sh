7d554c4124fb8cb79bbdd5314706d2b74909777f
Ihor Rusyn <0kaba0@gmail.com>
2026-06-16 17:26:24 +0200

    feat: optional debug logging via SRS_DEBUG env var

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -6,6 +6,7 @@ SECRET="${SRS_SECRET:-changeme}"
 MILTER_PORT="${SRS_MILTER_PORT:-10001}"
 SOCKETMAP_PORT="${SRS_SOCKETMAP_PORT:-10003}"
 REDIS_ADDR="${SRS_REDIS_ADDR:-}"
+DEBUG="${SRS_DEBUG:-off}"
 
 install -m 600 /dev/null /run/postsrsd.secret
 printf '%s\n' "${SECRET}" > /run/postsrsd.secret
@@ -19,6 +20,7 @@ socketmap = inet:0.0.0.0:${SOCKETMAP_PORT}
 chroot-dir = ""
 unprivileged-user = ""
 seccomp = off
+debug = ${DEBUG}
 CONF
 
 if [ -n "${REDIS_ADDR}" ]; then
